131a71e3991db6b27b3dd41eb27e4778f0f67bae
Josh <diab0022@algonquinlive.com>
2020-04-09 00:24:50 -0400

    Overwrite Josh

--- src/main/java/com/algonquincollege/cst8277/models/EmployeePojo_.java
+++ src/main/java/com/algonquincollege/cst8277/models/EmployeePojo_.java
@@ -6,14 +6,14 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2020-04-09T00:10:19.575-0400")
+@Generated(value="Dali", date="2020-04-09T00:20:36.641-0400")
 @StaticMetamodel(EmployeePojo.class)
 public class EmployeePojo_ extends PojoBase_ {
 	public static volatile SingularAttribute<EmployeePojo, String> firstName;
 	public static volatile SingularAttribute<EmployeePojo, String> lastName;
-	public static volatile SetAttribute<EmployeePojo, ProjectPojo> projects;
 	public static volatile ListAttribute<EmployeePojo, PhonePojo> phones;
 	public static volatile SingularAttribute<EmployeePojo, String> email;
 	public static volatile SingularAttribute<EmployeePojo, String> title;
 	public static volatile SingularAttribute<EmployeePojo, Double> salary;
+	public static volatile SetAttribute<EmployeePojo, ProjectPojo> projects;
 }
